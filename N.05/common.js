02831f5e4b39d5ab4843e69e1d0f87a633b6acf2
InvizzzZZ <dmitry.hrom@gmail.com>
2019-07-07 22:28:08 +0300

    N.05 update

--- N.05/common.js
+++ N.05/common.js
@@ -6,17 +6,36 @@ let arr = [5, 7,
 
 alert(treeSum(arr));
 
-function treeSum(arrParam, sumParam) {
-    let sum = sumParam || 0;
+// function treeSum(arrParam, sumParam) {
+//     let sum = sumParam || 0;
+//
+//     arrParam.forEach(function (item, i, arr) {
+//         if (typeof item !== 'object') {
+//             sum += item;
+//         } else {
+//             sum = treeSum(arr[i], sum);
+//         }
+//     });
+//
+//     return sum;
+//
+// }
+//
+
+function treeSum(arrParam) {
+    let sum =  0;
 
     arrParam.forEach(function (item, i, arr) {
         if (typeof item !== 'object') {
             sum += item;
         } else {
-            sum = treeSum(arr[i], sum);
+            sum += treeSum(arr[i]);
         }
     });
 
     return sum;
 
 }
+
+
+
