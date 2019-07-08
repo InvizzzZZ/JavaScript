0bfe9ca1769c3bdf283f64c5f89081bf363ae5e3
InvizzzZZ <dmitry.hrom@gmail.com>
2019-07-08 20:23:14 +0300

    A4 update #4

--- A4+/common.js
+++ A4+/common.js
@@ -2,15 +2,16 @@
 
 let userStr = prompt("Введите фразу на проверку на палиндром");
 
-alert(`${userStr} ${checkPalindrom(userStr.toLowerCase().replace(/[^а-яё]/g, '')) ? ' - является палиндромом' : ' - не является палиндромом'}`);
+alert(`${userStr} ${checkPalindrom(userStr.toLowerCase().replace(/[^а-дж-щыэ-я]/g, '')) ? ' - является палиндромом' : ' - не является палиндромом'}`);
 
 function checkPalindrom(str) {
-    if (str.charAt(0) !== str.charAt(str.length - 1)) {
+    if ((str.charAt(0) !== str.charAt(str.length - 1)) || str.length === 0) {
         return false;
 
     } else {
-        if (str.slice(1, str.length - 1).length > 0) {
-            return checkPalindrom(str.slice(1, str.length - 1));
+        let _str_ = str.slice(1, str.length - 1);
+        if (_str_.length > 0) {
+            return checkPalindrom(_str_);
         } else {
             return true;
         }
