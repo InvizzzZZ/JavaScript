b1123554ebfcfb16ce06360dd0042506babe238e
Hrom <dmitry.hrom@gmail.com>
2019-07-08 17:21:04 +0300

    А4+ update #2

--- A4+/common.js
+++ A4+/common.js
@@ -2,16 +2,19 @@
 
 let userStr = prompt("Введите фразу на проверку на палиндром");
 
-alert(userStr + checkPalindrom(userStr)? ' - является палиндромом' : ' - не является палиндромом');
+let str = userStr.toLowerCase().replace(/[^а-яё]/g, '');
+
+alert(`${userStr} ${checkPalindrom(str) ? ' - является палиндромом' : ' - не является палиндромом'}`);
 
 function checkPalindrom(str) {
-    
-    str = str.toLowerCase().replace(/[^а-яё]/g, '');
-
     if (str.charAt(0) !== str.charAt(str.length - 1)) {
         return false;
 
     } else {
-        return checkPalindrom(str.slice(1, str.length - 1));
+        if (str.slice(1, str.length - 1).length > 0) {
+            return checkPalindrom(str.slice(1, str.length - 1));
+        } else {
+            return true;
+        }
     }
 }
