1e5aca462af6fcee8b18415196ffc68f6338069f
Hrom <dmitry.hrom@gmail.com>
2019-07-09 09:49:08 +0300

    A4+ palindromE

--- A4+/common.js
+++ A4+/common.js
@@ -2,23 +2,23 @@
 
 let userStr = prompt("Введите фразу на проверку на палиндром");
 
-alert(`${userStr} ${checkPalindrom(userStr) ? ' - является палиндромом' : ' - не является палиндромом'}`);
+alert(`${userStr} ${checkPalindrome(userStr) ? ' - является палиндромом' : ' - не является палиндромом'}`);
 
-function checkPalindrom(str) {
+function checkPalindrome(str) {
     str.toLowerCase().replace(/[^а-яё]/g, '')
                      .replace('ё', 'е')
                      .replace('ъ', 'ь');
 
-    return isPalindrom(str);
+    return isPalindrome(str);
 
-    function isPalindrom(updateStr) {
+    function isPalindrome(updateStr) {
         if (updateStr.charAt(0) !== updateStr.charAt(updateStr.length - 1)) {
             return false;
 
         } else {
             let _str_ = updateStr.slice(1, updateStr.length - 1);
             if (_str_.length > 0) {
-                return isPalindrom(_str_);
+                return isPalindrome(_str_);
             } else {
                 return true;
             }
