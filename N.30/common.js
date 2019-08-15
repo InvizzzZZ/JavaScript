c001832c1f84ef1c95c08225d788a36c4a1efc8c
InvizzzZZ <dmitry.hrom@gmail.com>
2019-08-15 17:47:53 +0300

    N.30 document mousemove

--- N.30/common.js
+++ N.30/common.js
@@ -1,16 +1,9 @@
 'use strict';
 
-let elems = document.querySelectorAll('img');
-let _top = 0;
-let _left = 0;
+var elems = document.querySelectorAll('img');
 
-for(let i = 0; i < elems.length; i++){
+for (let i = 0; i < elems.length; i++) {
     elems[i].addEventListener('mousedown', _mouseDown, false);
-    elems[i].style.position = 'absolute';
-    elems[i].style.top = _top + 'px';
-    elems[i].style.left = _left + 'px';
-    _top += elems[i].offsetHeight + 20;
-    _left += elems[i].offsetWidth + 20;
 }
 
 var index = 1;
@@ -19,33 +12,43 @@ function _mouseDown(EO) {
     EO = EO || window.event;
     let elem = EO.target;
 
+    EO.preventDefault();
+
+    var elems = document.querySelectorAll('img');
+
+    for (let i = elems.length - 1; i >= 0; i--) {
+        elems[i].style.position = 'absolute';
+        elems[i].style.top = Math.round(+elems[i].offsetTop) + 'px';
+        elems[i].style.left = Math.round(+elems[i].offsetLeft) + 'px';
+    }
+
     var coords = getCoords(elem);
-    var shiftX = EO.pageX - coords.left;
-    var shiftY = EO.pageY - coords.top;
+    var shiftX = Math.round(EO.pageX - coords.left);
+    var shiftY = Math.round(EO.pageY - coords.top);
 
     elem.style.zIndex = index++;
 
-    elem.addEventListener('mousemove', _mouseMove, false);
+    // elem.addEventListener('mousemove', _mouseMove, false);
+    document.addEventListener('mousemove', _mouseMove, false);
 
     function _mouseMove(EO) {
         EO = EO || window.event;
-        let elem = EO.target;
 
         EO.preventDefault();
 
-        elem.style.left = EO.pageX - shiftX + 'px';
-        elem.style.top = EO.pageY - shiftY + 'px';
+        elem.style.left = Math.round(EO.pageX - shiftX) + 'px';
+        elem.style.top = Math.round(EO.pageY - shiftY) + 'px';
     }
 
     elem.addEventListener('mouseup', _mouseUp, false);
 
     function _mouseUp(EO) {
         EO = EO || window.event;
-        let elem = EO.target;
 
         EO.preventDefault();
 
-        elem.removeEventListener('mousemove', _mouseMove, false);
+        // elem.removeEventListener('mousemove', _mouseMove, false);
+        document.removeEventListener('mousemove', _mouseMove, false);
         elem.removeEventListener('mouseup', _mouseUp, false);
     }
 }
@@ -53,7 +56,7 @@ function _mouseDown(EO) {
 function getCoords(elem) {
     var box = elem.getBoundingClientRect();
     return {
-        top: box.top + pageYOffset,
-        left: box.left + pageXOffset
+        top: Math.round(box.top + pageYOffset),
+        left: Math.round(box.left + pageXOffset)
     };
 }
